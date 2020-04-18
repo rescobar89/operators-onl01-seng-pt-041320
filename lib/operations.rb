cf052b8e74304c70fe0fe7b3d906ee6733c53b5c
Rocio X Escobar <rocio.x.escobar@gmail.com>
2020-04-18 19:56:29 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,13 @@
 def unsafe?(speed)
   # return speed > 60 || speed < 40
-  if speed > 60
-    return true
-  elsif speed < 40
-    return true
-  else
-    return false
-  end
+  
+  # if speed > 60
+  #   return true
+  # elsif speed < 40
+  #   return true
+  # else
+  #   return false
+  # end
 end
 
 
