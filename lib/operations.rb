b8074f55acd775aa0578bf2ac8b650c33f2df16c
Rocio X Escobar <rocio.x.escobar@gmail.com>
2020-04-18 20:23:06 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -11,13 +11,13 @@ def unsafe?(speed)
   #   return false
   # end
   
-  # if speed is less than 60 dont return and this will evaluate line 16 to see if statement is true. Else it is false. You can swap line 13 and 16 and it will still work. 
-  if speed > 60 
-    return true
-  end
-  return speed < 40
+#   # if speed is less than 60 dont return and this will evaluate line 16 to see if statement is true. Else it is false. You can swap line 13 and 16 and it will still work. 
+#   if speed > 60 
+#     return true
+#   end
+#   return speed < 40
    
-end
+# end
 
 
 
